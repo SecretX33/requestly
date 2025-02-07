f38bdecabc89df58971c39af96e2faa63443a8b8
SecretX33 <4389757+secretx33@users.noreply.github.com>
2025-02-07 09:37:25 -0300

    wip

--- browser-extension/install.sh
+++ browser-extension/install.sh
@@ -12,9 +12,14 @@ rm -rf node_modules
 npm install
 cd ..
 
+echo -e "\n***** Installing MV2 dependencies *****"
+cd mv2
+rm -rf node_modules
+npm install
+cd ..
+
 echo -e "\n***** Installing MV3 dependencies *****"
 cd mv3
 rm -rf node_modules
 npm install
 cd ..
-
